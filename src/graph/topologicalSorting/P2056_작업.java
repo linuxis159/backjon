6b536e7c540aebca4e35fca36bce5fc62b7071e8
Ikgyeong.kim <dlrrud159@naver.com>
2024-01-18 18:13:48 +0900

    위상정렬

---
+++ src/graph/topologicalSorting/P2056_작업.java
@@ -0,0 +1,18 @@
+package graph.topologicalSorting;
+
+import java.util.Scanner;
+
+public class P2056_작업 {
+    public static Scanner sc = new Scanner(System.in);
+    public static int[] taskTime;
+    public static void main(String[] args){
+        int taskNum = sc.nextInt();
+        for(int i=0; i<taskNum; i++){
+            taskTime[i] = sc.nextInt();
+            int requiredTaskNum = sc.nextInt();
+            for(int j=0; j<requiredTaskNum; j++){
+
+            }
+        }
+    }
+}
