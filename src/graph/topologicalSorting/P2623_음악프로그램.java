a6d275a2ce9c66c6125c6dddb895b9e967cb46f2
ikgyoeng.kim <dlrrud159@gmail.com>
2024-01-24 22:46:41 +0900

    sort

--- src/graph/topologicalSorting/P2623_음악프로그램.java
+++ src/graph/topologicalSorting/P2623_음악프로그램.java
@@ -3,68 +3,78 @@ package graph.topologicalSorting;
 import java.util.*;
 
 public class P2623_음악프로그램 {
-       private static Scanner sc = new Scanner(System.in);
-       private static Map<Integer,Node> allNode = new HashMap();
-       private static Queue<Integer> queue = new LinkedList();
-       public static void main(String[] args){
-           int singerCnt = sc.nextInt();
-           int PDcnt = sc.nextInt();
+    private static Scanner sc = new Scanner(System.in);
+    private static Map<Integer,Node> allNode = new HashMap();
+    private static Queue<Integer> queue = new LinkedList();
+    private static Queue<Integer> resultQueue = new LinkedList();
+    public static void main(String[] args){
+        int singerCnt = sc.nextInt();
+        int PDcnt = sc.nextInt();
 
-           int from = sc.nextInt();
-           int to = 0;
-           while(sc.hasNext()) {
-               to = sc.nextInt();
-               System.out.println(from+","+to);
-               Node fromNode;
-               Node toNode;
-               if (allNode.get(from) == null) {
-                   fromNode = new Node(from);
-                   allNode.put(from, fromNode);
-               } else fromNode = allNode.get(from);
-               if (allNode.get(to) == null) {
-                   toNode = new Node(to);
-                   allNode.put(to, toNode);
-               } else toNode = allNode.get(to);
+        for(int i=0; i<singerCnt; i++){
+            allNode.put(i+1, new Node(i+1));
+        }
 
-               fromNode.edgeNodeNumList.add(to);
-               toNode.requiredNodeNumList.add(from);
-               from = to;
-           }
+        for(int i=0; i<PDcnt; i++){
+            int cnt = sc.nextInt();
+            int from = sc.nextInt();
+            for(int j=0; j<cnt-1; j++) {
+                int to = sc.nextInt();
 
+                Node fromNode = allNode.get(from);
+                Node toNode = allNode.get(to);
 
-           for(int nodeNum : allNode.keySet()){
-               Node node = allNode.get(nodeNum);
-               if(node.requiredNodeNumList.size() == 0) {
-                   queue.add(node.nodeNum);
-                   node.visit = true;
-               }
-           }
-
-           while(queue.size() != singerCnt){
-               int nodeNum = queue.peek();
-               Node thisNode =  allNode.get(nodeNum);
-               thisNode.visit = true;
-               for(int edgeNodeNum : thisNode.edgeNodeNumList){
-                   Node edgeNode = allNode.get(edgeNodeNum);
-                   edgeNode.requiredNodeNumList.remove(thisNode.nodeNum);
-                   if(edgeNode.requiredNodeNumList.size() == 0 && edgeNode.visit == false){
-                       queue.add(edgeNodeNum);
-                   }
-               }
-           }
-
-           for(int num : queue)
-               System.out.println(num);
+                fromNode.edgeNodeNumList.add(to);
+                toNode.requiredNodeNumList.add(from);
+                from = to;
+            }
+        }
 
 
-       }
-       private static class Node{
-           int nodeNum;
-           boolean visit = false;
-           private List<Integer> edgeNodeNumList = new ArrayList();
-           private List<Integer> requiredNodeNumList = new ArrayList();
-           private Node(int nodeNum){
-               this.nodeNum = nodeNum;
-           }
-       }
+
+        for(int nodeNum : allNode.keySet()){
+            Node node = allNode.get(nodeNum);
+            if(node.requiredNodeNumList.size() == 0) {
+                queue.add(node.nodeNum);
+                node.visit = true;
+            }
+        }
+
+        while(!queue.isEmpty()){
+            int nodeNum = queue.poll();
+            resultQueue.add(nodeNum);
+            Node thisNode =  allNode.get(nodeNum);
+            for(int edgeNodeNum : thisNode.edgeNodeNumList){
+                Node edgeNode = allNode.get(edgeNodeNum);
+                edgeNode.requiredNodeNumList.remove(thisNode.nodeNum);
+                if(edgeNode.requiredNodeNumList.size() == 0){
+                    if(edgeNode.visit) continue;
+                    else{
+                        edgeNode.visit = true;
+                        queue.add(edgeNodeNum);
+                    }
+                }
+            }
+        }
+
+        if(resultQueue.size() != singerCnt){
+            System.out.println(0);
+        }
+
+        else {
+            for (int num : resultQueue)
+                System.out.println(num);
+        }
+
+
+    }
+    private static class Node{
+        int nodeNum;
+        boolean visit = false;
+        private List<Integer> edgeNodeNumList = new ArrayList();
+        private Set<Integer> requiredNodeNumList = new HashSet();
+        private Node(int nodeNum){
+            this.nodeNum = nodeNum;
+        }
+    }
 }
