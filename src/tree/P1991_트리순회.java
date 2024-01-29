da409612677925a4bbeb21a83d0a0094f1e00894
Ikgyeong.kim <dlrrud159@naver.com>
2024-01-29 13:05:10 +0900

    위상정렬

---
+++ src/tree/P1991_트리순회.java
@@ -0,0 +1,82 @@
+package tree;
+
+import java.util.*;
+
+public class P1991_트리순회 {
+    private static Scanner sc = new Scanner(System.in);
+    private static Map<String,Node> allNodeMap = new HashMap();
+    public static void main(String[] args){
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            String nodeName = sc.next();
+            String leftNodeName = sc.next();
+            String rightNodeName = sc.next();
+
+            Node node;
+            Node leftNode;
+            Node rightNode;
+            if(leftNodeName.equals(".")) leftNode = null;
+            else if(allNodeMap.get(leftNodeName) == null) {
+                leftNode = new Node(leftNodeName, null, null);
+                allNodeMap.put(leftNodeName, leftNode);
+            }
+            else leftNode = allNodeMap.get(leftNodeName);
+
+            if(rightNodeName.equals(".")) rightNode = null;
+            else if(allNodeMap.get(rightNodeName) == null) {
+                rightNode = new Node(rightNodeName, null, null);
+                allNodeMap.put(rightNodeName, rightNode);
+            }
+            else rightNode = allNodeMap.get(rightNodeName);
+
+            if(allNodeMap.get(nodeName) == null) {
+                node = new Node(nodeName, leftNode, rightNode);
+                allNodeMap.put(nodeName, node);
+            }
+            else node = allNodeMap.get(nodeName);
+            node.leftNode = leftNode;
+            node.rightNode = rightNode;
+
+        }
+        Node rootNode = allNodeMap.get("A");
+        rootNode.travelPreorder(rootNode);
+        System.out.println();
+        rootNode.travelInorder(rootNode);
+        System.out.println();
+        rootNode.traverPostorder(rootNode);
+
+
+    }
+    private static class Node{
+        String my;
+        Node leftNode;
+        Node rightNode;
+        private Node(String my, Node leftNode, Node rightNode){
+            this.my = my;
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
+        }
+        void travelPreorder(Node node){
+            if(node == null)
+                return;
+            System.out.print(node.my);
+            travelPreorder(node.leftNode);
+            travelPreorder(node.rightNode);
+        }
+        void travelInorder(Node node){
+            if(node == null)
+                return;
+            travelPreorder(node.leftNode);
+            System.out.println(node.my);
+            travelPreorder(node.rightNode);
+        }
+        void traverPostorder(Node node){
+            if(node == null)
+                return;
+            travelPreorder(node.leftNode);
+            travelPreorder(node.rightNode);
+            System.out.println(node.my);
+        }
+
+    }
+}
