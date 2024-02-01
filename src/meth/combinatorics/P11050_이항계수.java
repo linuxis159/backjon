443caa6771da88fb76b6431e309d4d0ba96f6bd8
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-01 13:05:10 +0900

    이항계수

---
+++ src/meth/combinatorics/P11050_이항계수.java
@@ -0,0 +1,24 @@
+package meth.combinatorics;
+
+import java.util.Scanner;
+
+public class P11050_이항계수 {
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        if( K == 0  || N == K){
+            System.out.println(1);
+            return;
+        }
+        int denominator = getFactorial(N-K) * getFactorial(K);
+        int molecule = getFactorial(N);
+        System.out.println(molecule / denominator);
+
+
+    }
+    static int getFactorial(int number){
+        if(number == 1 || number == 0)  return number;
+        return number * getFactorial(number -1);
+    }
+}
