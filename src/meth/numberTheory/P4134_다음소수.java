0e3d85d3a8fa1eb838303752f1357e90085d0d71
ikgyoeng.kim <dlrrud159@gmail.com>
2024-02-01 20:39:42 +0900

    sort

--- src/meth/numberTheory/P4134_다음소수.java
+++ src/meth/numberTheory/P4134_다음소수.java
@@ -6,8 +6,6 @@ import java.util.Set;
 
 public class P4134_다음소수 {
     private static Scanner sc = new Scanner(System.in);
-    private static Set<Long> primeNumSet = new HashSet();
-    private static Set<Long> compositNumSet = new HashSet();
     public static void main(String[] args){
         int N = sc.nextInt();
         for(int i=0; i<N; i++){
@@ -21,15 +19,16 @@ public class P4134_다음소수 {
             for(long j=num;;j++){
                 int numSqrt = (int)Math.sqrt(j) + 1;
                 for(long k=2; k<=numSqrt; k++){
+                    if(j % k == 0) {
+                        break;
+                    }
                     if(k == numSqrt) {
                         System.out.println(j);
                         break loopOut;
                     }
-                    if(numSqrt % k == 0) {
-                        break;
-                    }
                 }
             }
+
         }
 
     }
