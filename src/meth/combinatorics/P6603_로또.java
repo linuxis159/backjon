a61a401f68efbab8e9d328e1dc801f7c9b34b96d
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-02 10:40:07 +0900

    이항계수

--- src/meth/combinatorics/P6603_로또.java
+++ src/meth/combinatorics/P6603_로또.java
@@ -10,11 +10,7 @@ public class P6603_로또 {
             int N = sc.nextInt();
             if(N == 0) return;
             int[] arr = new int[N];
-            else {
-                for(int i=0; i<N; i++){
 
-                }
-            }
         }
 
     }
