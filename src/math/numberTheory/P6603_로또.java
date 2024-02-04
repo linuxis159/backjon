20acf57e1101c491a2bbcef4d3a021b561a54ebc
ikgyoeng.kim <dlrrud159@gmail.com>
2024-02-04 18:51:09 +0900

    sort

--- src/math/numberTheory/P6603_로또.java
+++ src/math/numberTheory/P6603_로또.java
@@ -1,16 +1,14 @@
 package math.numberTheory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class P6603_로또 {
     private static Scanner sc = new Scanner(System.in);
     static List<Integer> numList;
-    static int[] result;
+    static List<Integer> result;
     static boolean[] visit;
     static int N;
     static int seq = 5;
@@ -25,63 +23,32 @@ public class P6603_로또 {
                 for (int i = 0; i < N; i++) {
                     numList.add(sc.nextInt());
                 }
-/*                if(numList.size() == 6){
-                    numList.stream().forEach(num -> System.out.print(num + " "));
-                    System.out.println();
-                    return;
-                }*/
-                for (int i = 0; i <= numList.size() - 6; i++) {
-                    result = new int[6];
+               // for (int i = 0; i <= numList.size() - 6; i++) {
+                    result = new Stack();
                     visit = new boolean[N];
-                    dfs(i);
-                }
-                System.out.println();
-
+                    dfs(0);
+                //}
             }
         }
     }
-        private static void dfs(int start){
-            for(int i=0; i<5; i++){
-                int exclude = -1;
-                if(i != 0) {
-                    exclude = 5-i+start;
-                }
-                int k = 0;
-                for(int j=start; j<numList.size(); j++){
-                    if (exclude != j) {
-                        result[k] = numList.get(j);
-                        k++;
-                        if (k == 6) {
-                            String resultString = Arrays.stream(result)
-                                    .mapToObj(String::valueOf)
-                                    .collect(Collectors.joining(" "));
-                            System.out.println(resultString);
-                            k--;
-                        }
-                    }
-                }
-            }
-
-
-
-        }
-/*    private static void dfs(int start, int depth){
-        if (depth == 6) {
-            Arrays.stream(result).forEach(num -> System.out.print(num + " "));
-            System.out.println();
+    private static void dfs(int start){
+        if (result.size() == 6) {
+            String resultString = result.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(" "));
+            System.out.println(resultString);
             return;
         }
 
         for (int i = start; i < numList.size(); i++) {
             if (visit[i] == false) {
                 visit[i] = true;
-                result[depth] = numList.get(i);
-                dfs(start, depth + 1);
+                result.add(numList.get(i));
+                dfs(i + 1);
+                result.remove(result.size()-1);
+                visit[i] = false;
             }
         }
-
-
-
-    }*/
+    }
 
 }
