08fc5c50b484be345563ee2c9a0933a3272720a0
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-06 18:04:07 +0900

    이항계수

---
+++ src/graph/dijkstra/P11779_최소비용구하기2.java
@@ -0,0 +1,20 @@
+package graph.dijkstra;
+
+import java.util.Scanner;
+
+public class P11779_최소비용구하기2 {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        for(int i=0; i<M; i++){
+            int from    = sc.nextInt();
+            int to      = sc.nextInt();
+            int weight  = sc.nextInt();
+        }
+    }
+    private static class Node{
+
+    }
+}
