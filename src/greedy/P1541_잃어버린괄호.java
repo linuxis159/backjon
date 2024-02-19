50eeb22a22aa92d96e1a31d86f1a5258058fc3f2
ikgyoeng.kim <dlrrud159@gmail.com>
2024-02-19 21:51:28 +0900

    sort

--- src/greedy/P1541_잃어버린괄호.java
+++ src/greedy/P1541_잃어버린괄호.java
@@ -6,25 +6,49 @@ public class P1541_잃어버린괄호 {
     static Scanner sc = new Scanner(System.in);
     static List<Character> expression = new ArrayList();
     static List<Integer> number = new ArrayList();
+    static int minusValue = 0;
+    static int plusValue = 0;
     public static void main(String[] args){
         String expression = sc.next();
         boolean zeroSkipFlag = true;
-        boolean startMinusFlag = false;
+        boolean metMinusFlag = false;
+        String numberString = "";
         for(int i=0; i<expression.length(); i++){
             char charecter = expression.charAt(i);
             if(charecter == '0' && zeroSkipFlag)
                 continue;
-            else if(charecter == '-' && !startMinusFlag){
-
+            else if(charecter == '+' && !metMinusFlag){
+                plusValue += Integer.parseInt(numberString);
+                zeroSkipFlag = true;
+                numberString = "";
             }
-            else if(charecter == '+' && startMinusFlag){
-                
+            else if(charecter == '+' && metMinusFlag){
+                minusValue += Integer.parseInt(numberString);
+                zeroSkipFlag = true;
+                numberString = "";
             }
-            else if(charecter == '+'){
-
+            else if(charecter == '-' && !metMinusFlag){
+                plusValue += Integer.parseInt(numberString);
+                metMinusFlag = true;
+                zeroSkipFlag = true;
+                numberString = "";
             }
-
-
+            else if(charecter == '-' && metMinusFlag){
+                minusValue += Integer.parseInt(numberString);
+                zeroSkipFlag = true;
+                numberString = "";
+            }
+            else if(i+1 == expression.length() && metMinusFlag){
+                minusValue += Integer.parseInt(numberString + charecter);
+            }
+            else if(i+1 == expression.length() && !metMinusFlag){
+                plusValue += Integer.parseInt(numberString + charecter);
+            }
+            else {
+                zeroSkipFlag = false;
+                numberString += charecter;
+            }
         }
+        System.out.println(plusValue - minusValue);
     }
 }
