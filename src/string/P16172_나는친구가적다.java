5a04da1fb6622415002ca08fed478001a230d9b4
ikgyoeng.kim <dlrrud159@gmail.com>
2024-02-27 00:24:28 +0900

    sort

--- src/string/P16172_나는친구가적다.java
+++ src/string/P16172_나는친구가적다.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class P16172_나는친구가적다 {
     static Scanner sc = new Scanner(System.in);
     static int[] patternJumpTable;
+    static char[] skipChar = {
+            '0','1','2','3','4','5','6','7','8','9'
+    };
     public static void main(String[] args){
         String target = sc.next();
         String pattern = sc.next();
@@ -12,10 +15,36 @@ public class P16172_나는친구가적다 {
 
         int j =0;
         for(int i=1; i<patternJumpTable.length; i++){
+
             while(j > 0 && pattern.charAt(i) != pattern.charAt(j)){
                 j = patternJumpTable[j-1];
             }
+            if(pattern.charAt(i) == pattern.charAt(j)){
+                patternJumpTable[j] = ++j;
+            }
         }
+        j = 0;
+
+        for(int i=0; i<target.length(); i++){
+            boolean flag = false;
+
+            if(48 <= target.charAt(i) && target.charAt(i) <= 57)
+                continue;
+
+
+            while(j > 0 && target.charAt(i) != pattern.charAt(j)){
+                j = patternJumpTable[j-1];
+            }
+
+            if(pattern.length()-1 == j){
+                System.out.println(1);
+                return;
+            }
+            else if(target.charAt(i) == pattern.charAt(j)){
+                 ++j;
+            }
+        }
+        System.out.println(0);
     }
 
 }
