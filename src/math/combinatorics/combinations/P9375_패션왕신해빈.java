9e7fd8803e35db93d26dad89fd82c9c092ba6588
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-27 16:25:07 +0900

    이항계수

--- src/math/combinatorics/combinations/P9375_패션왕신해빈.java
+++ src/math/combinatorics/combinations/P9375_패션왕신해빈.java
@@ -1,7 +1,74 @@
 package math.combinatorics.combinations;
 
+import java.util.*;
+
+/*    문제
+해빈이는 패션에 매우 민감해서 한번 입었던 옷들의 조합을 절대 다시 입지 않는다.
+예를 들어 오늘 해빈이가 안경, 코트, 상의, 신발을 입었다면,
+다음날은 바지를 추가로 입거나 안경대신 렌즈를 착용하거나 해야한다.
+해빈이가 가진 의상들이 주어졌을때 과연 해빈이는 알몸이 아닌 상태로 며칠동안 밖에 돌아다닐 수 있을까?
+
+입력
+첫째 줄에 테스트 케이스가 주어진다. 테스트 케이스는 최대 100이다.
+
+각 테스트 케이스의 첫째 줄에는 해빈이가 가진 의상의 수 n(0 ≤ n ≤ 30)이 주어진다.
+다음 n개에는 해빈이가 가진 의상의 이름과 의상의 종류가 공백으로 구분되어 주어진다. 같은 종류의 의상은 하나만 입을 수 있다.
+모든 문자열은 1이상 20이하의 알파벳 소문자로 이루어져있으며 같은 이름을 가진 의상은 존재하지 않는다.
+
+출력
+각 테스트 케이스에 대해 해빈이가 알몸이 아닌 상태로 의상을 입을 수 있는 경우를 출력하시오.
+
+
+예제 입력 1
+2
+
+3
+hat headgear
+sunglasses eyewear
+turban headgear
+
+3
+mask face
+sunglasses face
+makeup face
+
+예제 출력 1
+5
+3
+*/
 public class P9375_패션왕신해빈 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++){
+            int N = sc.nextInt();
+            Map<String, List<String>> map = new HashMap<>();
+            for(int j=0; j<N; j++) {
+                String name = sc.next();
+                String type = sc.next();
+                if (map.get(type) == null) {
+                    List<String> list = new ArrayList();
+                    list.add(name);
+                    map.put(type, list);
+                } else {
+                    List list = map.get(type);
+                    list.add(name);
+                }
+            }
+            int result = 0;
+            int init = 1;
+
+            List<Integer> numList = new ArrayList();
+            for(String type : map.keySet()){
+                List<String> list = map.get(type);
+                init *= list.size()+1;
+            }
+            init -= 1;
+            System.out.println(init);
+
+
+
+        }
 
     }
 }
