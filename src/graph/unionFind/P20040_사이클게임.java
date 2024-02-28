3062e38537568059ceab98c73a54039c213658cc
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-28 17:34:30 +0900

    이항계수

---
+++ src/graph/unionFind/P20040_사이클게임.java
@@ -0,0 +1,19 @@
+package graph.unionFind;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P20040_사이클게임 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+        }
+    }
+}
---
+++ src/math/combinatorics/combinations/P10164_격자상의경로.java
@@ -0,0 +1,7 @@
+package math.combinatorics.combinations;
+
+public class P10164_격자상의경로 {
+    public static void main(String[] args){
+
+    }
+}
