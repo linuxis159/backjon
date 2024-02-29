9b7f9eb4f5da69648bc84d4891063956fc2dcfbd
Ikgyeong.kim <dlrrud159@naver.com>
2024-02-29 16:13:44 +0900

    이항계수

---
+++ src/graph/minimumSpanningTree/P1922_네트워크연결.java
@@ -0,0 +1,21 @@
+package graph.minimumSpanningTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1922_네트워크연결 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+
+        int V = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+        for(int i=0; i<E; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+        }
+
+    }
+
+}
--- src/math/cryptography/RSA/p15995_잉여역수구하기.java
+++ src/math/cryptography/RSA/p15995_잉여역수구하기.java
@@ -3,7 +3,6 @@ package math.cryptography.RSA;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class p15995_잉여역수구하기 {
