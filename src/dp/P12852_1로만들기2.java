6face599a4225683c7b866a62aedddc0ffab20c0
ikgyoeng.kim <dlrrud159@gmail.com>
2024-03-05 23:46:19 +0900

    sort

--- src/dp/P12852_1로만들기2.java
+++ src/dp/P12852_1로만들기2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P12852_1로만들기2 {
     static Scanner sc = new Scanner(System.in);
     static int dp[];
+    static int dp2[];
     public static void main(String[] args){
         int N = sc.nextInt();
         if(N == 1){
@@ -20,24 +21,41 @@ public class P12852_1로만들기2 {
             return;
         }
         dp = new int[N+1];
+        dp2 = new int[N+1];
         dp[1] = 0;
+        dp2[1] = 0;
         dp[2] = 1;
+        dp2[2] = 1;
         dp[3] = 1;
+        dp2[3] = 1;
         for(int i=4; i<=N; i++){
             int resultOne = 9000000;
             int resultTwo = 9000000;
             int resultThree = 9000000;
             int resultFour =9999999;
             if(i % 2 == 0){
-                resultOne = dp[i / 2] + 1;
+                if(dp[i / 2] > dp[i-1]){
+                    dp[i] = dp[i-1] + 1;
+                    dp2[i] = i-1;
+                }
+                else{
+                    dp[i] = dp[i / 2] + 1;
+                    dp2[i] = i/2;
+                }
             }
             if(i % 3 == 0){
-                resultTwo = dp[i / 3] + 1;
+                if(dp[i / 3] > dp[i-1]){
+                    dp[i] = dp[i-1] + 1;
+                    dp2[i] = i-1;
+                }
+                else{
+                    dp[i] = dp[i / 3] + 1;
+                    dp2[i] = i/3;
+                }
             }
 
-            resultThree = dp[i-1] + 1;
-            resultFour = dp[i-2] + 2;
-            dp[i] = Math.min(Math.min(Math.min(resultOne, resultTwo),resultThree),resultFour);
+
+
         }
         System.out.println(dp[N]);
     }
