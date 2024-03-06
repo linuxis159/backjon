ff9e45cb1f2a57ccf5ad6f69678a9914ece0c804
ikgyoeng.kim <dlrrud159@gmail.com>
2024-03-06 23:07:34 +0900

    sort

--- src/dp/P12852_1로만들기2.java
+++ src/dp/P12852_1로만들기2.java
@@ -10,18 +10,21 @@ public class P12852_1로만들기2 {
         int N = sc.nextInt();
         if(N == 1){
             System.out.println(0);
+            System.out.print(1);
             return;
         }
         else if(N == 2){
             System.out.println(1);
+            System.out.print(2 + " " + 1);
             return;
         }
         else if(N == 3){
             System.out.println(1);
+            System.out.print(3 + " " + 1);
             return;
         }
         dp = new int[N+1];
-        dp2 = new int[N+1];
+        dp2= new int[N+1];
         dp[1] = 0;
         dp2[1] = 0;
         dp[2] = 1;
@@ -32,31 +35,31 @@ public class P12852_1로만들기2 {
             int resultOne = 9000000;
             int resultTwo = 9000000;
             int resultThree = 9000000;
-            int resultFour =9999999;
             if(i % 2 == 0){
-                if(dp[i / 2] > dp[i-1]){
-                    dp[i] = dp[i-1] + 1;
-                    dp2[i] = i-1;
-                }
-                else{
-                    dp[i] = dp[i / 2] + 1;
-                    dp2[i] = i/2;
-                }
+                resultOne = dp[i / 2] + 1;
             }
             if(i % 3 == 0){
-                if(dp[i / 3] > dp[i-1]){
-                    dp[i] = dp[i-1] + 1;
-                    dp2[i] = i-1;
-                }
-                else{
-                    dp[i] = dp[i / 3] + 1;
-                    dp2[i] = i/3;
-                }
+                resultTwo = dp[i / 3] + 1;
             }
 
-
-
+            resultThree = dp[i-1] + 1;
+            dp[i] = Math.min(Math.min(resultOne, resultTwo),resultThree);
+            if(dp[i] == resultOne){
+                dp2[i] = i/2;
+            }
+            else if(dp[i] == resultTwo){
+                dp2[i] = i/3;
+            }
+            else{
+                dp2[i] = i-1;
+            }
         }
         System.out.println(dp[N]);
+        for(int i = N; i != 1; ){
+            System.out.print(i +" ");
+            i = dp2[i];
+        }
+        System.out.print(1);
+
     }
 }
